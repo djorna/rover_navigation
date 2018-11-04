00303f8303855eea821993755cef99e47926feb7
David Jorna <davidjorna@gmail.com>
2018-11-04 12:35:43 -0500

    Removed catkin_generated

--- catkin_generated/pkg.installspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/usr/local/include".split(';') if "/usr/local/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "grid_map_core;grid_map_ros;grid_map_msgs;roscpp;std_msgs;pcl_ros".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lpath_planning".split(';') if "-lpath_planning" != "" else []
-PROJECT_NAME = "path_planning"
-PROJECT_SPACE_DIR = "/usr/local"
-PROJECT_VERSION = "0.0.0"
