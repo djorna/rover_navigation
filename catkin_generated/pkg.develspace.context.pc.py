6bfc77f24231a2a21c720a4cc7fdbf7db95d794e
David Jorna <davidjorna@gmail.com>
2018-11-04 12:30:49 -0500

    Removed catkin_generated

--- catkin_generated/pkg.develspace.context.pc.py
+++
@@ -1,8 +0,0 @@
-# generated from catkin/cmake/template/pkg.context.pc.in
-CATKIN_PACKAGE_PREFIX = ""
-PROJECT_PKG_CONFIG_INCLUDE_DIRS = "/home/djorna/catkin_ws/src/path_planning/include".split(';') if "/home/djorna/catkin_ws/src/path_planning/include" != "" else []
-PROJECT_CATKIN_DEPENDS = "grid_map_core;grid_map_ros;grid_map_msgs;roscpp;std_msgs;pcl_ros".replace(';', ' ')
-PKG_CONFIG_LIBRARIES_WITH_PREFIX = "-lpath_planning".split(';') if "-lpath_planning" != "" else []
-PROJECT_NAME = "path_planning"
-PROJECT_SPACE_DIR = "/home/djorna/catkin_ws/src/path_planning/devel"
-PROJECT_VERSION = "0.0.0"
